11c7cf4500db68dcdd60662e3e3e1d1e2b8d9294
mrsimpson <github@beimir.net>
2019-10-09 16:41:52 +0200

    Restart docker container if stopped

--- startsap.sh
+++ startsap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 DOCKER_PROCESS=$( docker ps | grep nwabap  | sed 's/ .*//' )
 
+if [ !DOCKER_PROCESS ]; then
+    DOCKER_PROCESS=$(docker container ls --all  | grep nwabap  | sed 's/ .*//' )
+    docker restart $DOCKER_PROCESS
+fi
+
 docker exec $DOCKER_PROCESS "/usr/sbin/uuidd"
 docker exec --user npladm $DOCKER_PROCESS /bin/bash -lc "/usr/sap/NPL/SYS/exe/uc/linuxx86_64/startsap ALL"
